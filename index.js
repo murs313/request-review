a217e92935d963b37a04c752d14752aaa9d2b39e
murs313 <seika.murata@zozo.com>
2020-12-10 12:15:45 +0900

    Delete logging github context

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ try {
   const numberOfPullRequest = core.getInput('number-of-pull-request');
   console.log(numberOfPullRequest);
   console.log(`Hello ${numberOfPullRequest}!`);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
