dcf8fa190fa8e60079abcef7e5bd79528bfe357d
murs313 <seika.murata@zozo.com>
2020-12-10 18:04:40 +0900

    Fix

--- dist/index.js
+++ dist/index.js
@@ -12,16 +12,10 @@ try {
   const token = process.env["GITHUB_TOKEN"] || core.getInput("token");
   const octokit = new github.getOctokit(token);
   const githubContext = github.context;
-  const pullRequestNumber = core.getInput('pull-request-number') || githubContext.payload.pull_request.number;
 
-  let reviewer = null;
-  if (core.getInput('reviewers') == '' ) {
-    reviewer = githubContext.payload.pull_request.number;
-  } else {
-    reviewer = core.getInput('reviewers').split(" ")
-  }
+  const pullRequestNumber = core.getInput('pull-request-number');
+  const reviewers = core.getInput('reviewers').split(" ")
 
-  console.log(core.getInput('reviewers'));
   console.log(`Request review from ${reviewers}`);
 
   octokit.pulls.requestReviewers({
@@ -29,8 +23,6 @@ try {
     pull_number: pullRequestNumber,
     reviewers: reviewers,
   });
-
-  console.log(`Success!`);
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -5,16 +5,10 @@ try {
   const token = process.env["GITHUB_TOKEN"] || core.getInput("token");
   const octokit = new github.getOctokit(token);
   const githubContext = github.context;
-  const pullRequestNumber = core.getInput('pull-request-number') || githubContext.payload.pull_request.number;
 
-  let reviewer = null;
-  if (core.getInput('reviewers') == '' ) {
-    reviewer = githubContext.payload.pull_request.number;
-  } else {
-    reviewer = core.getInput('reviewers').split(" ")
-  }
+  const pullRequestNumber = core.getInput('pull-request-number');
+  const reviewers = core.getInput('reviewers').split(" ")
 
-  console.log(core.getInput('reviewers'));
   console.log(`Request review from ${reviewers}`);
 
   octokit.pulls.requestReviewers({
@@ -22,8 +16,6 @@ try {
     pull_number: pullRequestNumber,
     reviewers: reviewers,
   });
-
-  console.log(`Success!`);
 } catch (error) {
   core.setFailed(error.message);
 }
