2dc292795d8c680cbdec9a15bd919e6c26e9c022
murs313 <seika.murata@zozo.com>
2020-12-10 12:10:58 +0900

    Tmp: log numberOfPullRequest

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const numberOfPullRequest = core.getInput('number-of-pull-request');
+  console.log(numberOfPullRequest);
   console.log(`Hello ${numberOfPullRequest}!`);
-
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
